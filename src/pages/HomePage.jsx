d7e8fe02e6dd7bca1c9a92d2c096cc9cf3b53535
Gabriel <gabrielfernandez090997@gmail.com>
2025-08-18 18:14:52 -0300

    Se agregó el componente de tecnologias a la homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import { Container } from "react-bootstrap";
 import Inicio from "../components/inicio/Inicio";
 import SobreMi from "../components/sobremi/SobreMi";
 import "./HomePage.css";
+import Tecnologias from "../components/tecnologias/Tecnologias";
 
 const HomePage = () => {
   return (
     <Container fluid className="homepage">
       <Inicio />
       <SobreMi />
+      <Tecnologias />
     </Container>
   );
 };
