c49704e032d83194d267537b526f9773fc973378
Gabriel <gabrielfernandez090997@gmail.com>
2026-03-07 16:27:09 -0300

    Se creó la carpeta data donde se guardará la informacion de las cards

---
+++ src/data/projectsData.js
@@ -0,0 +1,40 @@
+const proyectos = [
+  {
+    id: 1,
+    title: "Veterinaria",
+    img: "/Veterinaria.png",
+    description:
+      "Aplicación web para la gestión de una veterinaria desarrollada en equipo. Permite administrar usuarios, mascotas, servicios y pagos en línea mediante integración con Mercado Pago.",
+    badgeImg: "Fullstack",
+    badgeM: "MongoDB",
+    badgeE: "Express",
+    badgeR: "React",
+    badgeN: "Node.js",
+  },
+  {
+    id: 2,
+    title: "E-commerce",
+    img: "/Ecommerce.png",
+    description:
+      "Proyecto frontend de un ecommerce desarrollado en equipo con React. Incluye catálogo de productos, localstorage, filtros por categorías y marcas, y un diseño responsivo con Bootstrap.",
+    badgeImg: "Frontend",
+    badgeR: "React",
+    badgeB: "Bootstrap",
+    badgeJ: "JavaScript",
+    badgeC: "CSS",
+  },
+  {
+    id: 3,
+    title: "Estudio Jurídico",
+    img: "/Estudio Juridico.png",
+    description:
+      "Sitio web institucional desarrollado para un estudio jurídico. Permite presentar información del estudio, áreas de práctica y medios de contacto, ofreciendo una presencia profesional y accesible para clientes potenciales.",
+    badgeImg: "Frontend",
+    badgeR: "React",
+    badgeB: "Bootstrap",
+    badgeJ: "JavaScript",
+    badgeC: "CSS",
+  },
+];
+
+export default proyectos;
